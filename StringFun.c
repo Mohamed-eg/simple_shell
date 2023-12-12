c2788ba4dbfce28eea9fe14044840a23ea537cd4
Mohamed-eg <mhmdalshayb39@gmail.com>
2023-12-12 22:42:27 +0200

    unit_test_stringFun

--- StringFun.c
+++ StringFun.c
@@ -1,113 +1,128 @@
 #include "header.h"
 
 /**
- * getStringLength - returns the length of a string
- * @s: the string whose length to check
+ * getStringLength - Calculates the length of a string.
+ * @str_ch: The input string.
  *
- * Return: integer length of string
+ * Return: The length of the string. Returns 0 if the string is NULL.
  */
-int getStringLength(char *s)
+int getStringLength(char *str_ch)
 {
-	int i = 0;
-
-	if (!s)
+	int length = 0;
+	/*Check if the input string is NULL*/
+	if (!str_ch)
 		return (0);
-
-	while (*s++)
-		i++;
-	return (i);
+	/*Iterate through the string until the null terminator is encountered*/
+	while (*str_ch++)
+		length++;
+	return (length);
 }
 
 /**
- * compareStrings - performs lexicogarphic comparison of two strangs.
- * @s1: the first strang
- * @s2: the second strang
+ * compareStrings - Compares two strings lexicographically.
+ * @strone: The first string to compare.
+ * @strtwo: The second string to compare.
  *
- * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
+ * Return:
+ * - 0 if the strings are equal.
+ * - A negative value if strone is less than strtwo.
+ * - A positive value if strone is greater than strtwo.
  */
-int compareStrings(char *s1, char *s2)
+int compareStrings(char *strone, char *strtwo)
 {
-	while (*s1 && *s2)
+	/*Iterate through both strings*/
+	while (*strone && *strtwo)
 	{
-		if (*s1 != *s2)
-			return (*s1 - *s2);
-		s1++;
-		s2++;
+		/*Compare characters at the current position*/
+		if (*strone != *strtwo)
+			return (*strone - *strtwo);
+		/*Move to the next character in both strings*/
+		strone++;
+		strtwo++;
 	}
-	if (*s1 == *s2)
+	/*Check if both strings are equal*/
+	if (*strone == *strtwo)
 		return (0);
-	else
-		return (*s1 < *s2 ? -1 : 1);
+	else/*Return -1 if s1 is less, 1 if s1 is greater*/
+		return (*strone < *strtwo ? -1 : 1);
 }
 
 /**
- * startsWith - checks if needle starts with haystack
- * @haystack: string to search
- * @needle: the substring to find
+ * startsWith - Checks if a string starts with a given prefix.
+ * @check_str: The string to check if it starts with the prefix.
+ * @prefix_in: The prefix to check for at the beginning of the check_str.
  *
- * Return: address of next_node char of haystack or NULL
+ * Return:
+ * - A pointer to the remaining part of the check_str if it starts with the prefix.
+ * - NULL if the check_str does not start with the prefix.
  */
-char *startsWith(const char *haystack, const char *needle)
+char *startsWith(const char *check_str, const char *prefix_in)
 {
-	while (*needle)
-		if (*needle++ != *haystack++)
-			return (NULL);
-	return ((char *)haystack);
+	/*Iterate through the characters in the prefix*/
+	while (*prefix_in)
+		/*Compare characters at the current position*/
+		if (*prefix_in++ != *check_str++)
+			return (NULL);/*Return NULL if the characters do not match*/
+	/*Return a pointer to the remaining part of check_str*/
+	return ((char *)check_str);
 }
 
 /**
- * concatenetStrings - concatenates two strings
- * @dest: the destination buffer
- * @src: the source buffer
+ * concatStrings - Concatenates two strings.
+ * @destination: The destination string.
+ * @source: The source string to be concatenated to the destination.
  *
- * Return: pointer to destination buffer
+ * Return: A pointer to the concatenated string (destination).
  */
-char *concatenetStrings(char *dest, char *src)
+char *concatenetStrings(char *destination, char *source)
 {
-	char *ret = dest;
+	char *res = destination;
 
-/* Move dest pointer to the end of the destination string*/
-	while (*dest)
-		dest++;
+/* Move destination pointer to the end of the destination string*/
+	while (*destination)
+		destination++;
 
-/* Copy characters from src to dest */
-	while (*src)
-		*dest++ = *src++;
+/* Copy characters from source to destination */
+	while (*source)
+		*destination++ = *source++;
 
-	*dest = *src;
+	*destination = *source;
 /*
-* After the loops, the null from src is
+* After the loops, the null from source is
 * copieing to the end of the concatenated string
 */
-	return (ret);
+	return (res);
 }
 
 /**
- **StringCopy - copies a string
- *@dest: the destination string to be copied to
- *@src: the source string
- *@n: the amount of characters to be copied
- *Return: the concatenated string
+ * StringCopy - Copies at most max_char characters from source to destination.
+ * @destination: The destination string.
+ * @source: The source string to be copied.
+ * @max_char: The maximum number of characters to copy.
+ *
+ * Return: A pointer to the destination string.
  */
-char *StringCopy(char *dest, char *src, int n)
+char *StringCopy(char *destination, char *source, int max_char)
 {
-	int i, j;
-	char *s = dest;
+	int x, y;
+	char *str_ch = destination;
 
-	i = 0;
-	while (src[i] != '\0' && i < n - 1)
+	x = 0;
+	/*Copy at most n-1 characters from source to destination*/
+	while (source[x] != '\0' && x < max_char - 1)
 	{
-		dest[i] = src[i];
-		i++;
+		destination[x] = source[x];
+		x++;
 	}
-	if (i < n)
+	/*If i is less than n, fill the remaining characters with null*/
+	if (x < max_char)
 	{
-		j = i;
-		while (j < n)
+		y = x;
+		while (y < max_char)
 		{
-			dest[j] = '\0';
-			j++;
+			destination[y] = '\0';
+			y++;
 		}
 	}
-	return (s);
+	return (str_ch);
 }
