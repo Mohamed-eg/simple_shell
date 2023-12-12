923c4297d8372d54fc1c516c99d9b3ce9ac1c1e5
Mohamed-eg <mhmdalshayb39@gmail.com>
2023-12-12 23:17:24 +0200

    unit_test_MemoryFun

--- MemoryFun2.c
+++ MemoryFun2.c
@@ -1,18 +1,21 @@
 #include "header.h"
 
 /**
- * freePointers - frees a pointer and NULLs the address
- * @Pointer: address of the pointer to free
+ * freePointers - Frees the memory pointed to by a pointer and sets it to NULL.
+ * @Pointer: A pointer to the memory to be freed.
  *
- * Return: 1 if freed, otherwise 0.
+ * Return:
+ * - 1 if the memory was successfully freed.
+ * - 0 if the pointer is NULL or points to NULL.
  */
 int freePointers(void **Pointer)
 {
+	/*Check if the pointer is not NULL and points to a valid memory location*/
 	if (Pointer && *Pointer)
 	{
-		free(*Pointer);
-		*Pointer = NULL;
-		return (1);
+		free(*Pointer);/*Free the memory*/
+		*Pointer = NULL;/*Set the pointer to NULL*/
+		return (1);/* Return 1 indicating success*/
 	}
-	return (0);
+	return (0);/*Return 0 indicating failure*/
 }
